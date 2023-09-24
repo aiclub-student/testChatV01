a96c59b311c68577082192a6f03ec0aa5f6c99cd
Danika Gupta <danika@gprof.com>
2023-09-23 20:47:48 -0700

    Initial Commit

---
+++ testChatV01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testChatV01
+//
+//  Created by Amit Gupta on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testChatV01/testChatV01App.swift
@@ -0,0 +1,17 @@
+//
+//  testChatV01App.swift
+//  testChatV01
+//
+//  Created by Amit Gupta on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct testChatV01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
